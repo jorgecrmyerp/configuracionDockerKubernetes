d2df86e7b202753cc39e90b678b7eadea6ac384d
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-09 13:15:36 +0100

    mi.,  9 de nov. de 2022 13:15:342022-11-09 igualamos repositorios

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-09 ++configserver,eureka y gateway"
+message="$(date)2022-11-09 igualamos repositorios"
 fi
 
 start=`date +%s`
