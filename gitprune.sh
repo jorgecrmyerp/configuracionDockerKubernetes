768eb852c58475ffa93a960201cfbb71dabf2b5a
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-11 10:52:27 +0100

    vi., 11 de nov. de 2022 10:52:252022-11-11 junit alumno

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-10 EMPIEZO JENKINS"
+message="$(date)2022-11-11 junit alumno"
 fi
 
 start=`date +%s`
