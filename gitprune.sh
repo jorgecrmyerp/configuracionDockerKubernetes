6914230056e775d7c623b1a15c019c246b1028a5
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-13 11:02:54 +0100

    do., 13 de nov. de 2022 11:02:522022-11-13 alumno como servicio externo

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-11 junit alumno"
+message="$(date)2022-11-13 alumno como servicio externo"
 fi
 
 start=`date +%s`
