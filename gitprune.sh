248ae9d7bb5736424d83f10dfd4146c097fac133
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-17 13:03:33 +0100

    ju., 17 de nov. de 2022 13:03:312022-11-17 INTENTO DE SUBIDA

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-11-16 FUNCIONA OK"
+message="$(date)2022-11-17 INTENTO DE SUBIDA"
 fi
 
 start=`date +%s`
